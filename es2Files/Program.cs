693e59de05474bac85a612952372b6f509113869
belladonna.giacomo <belladonna.giacomo@HP-003>
2026-04-20 09:04:54 +0200

    mod

--- es2Files/Program.cs
+++ es2Files/Program.cs
@@ -9,8 +9,27 @@ namespace es2Files
             string[] prezziArray = { "19.99", "23.95", "5.50", "15.99", "10.50" };
             string prezzi = "prezzi.dat";
 
+            if(File.Exists(prezzi))
+            {
+                Console.WriteLine("Cosa vuoi fare: ");
+                Console.WriteLine("1) Sovrascrivere il file");
+                Console.WriteLine("2) Aggiungere le righe");
+                int scelta = Convert.ToInt32(Console.ReadLine());
+                if(scelta == 1)
+                {
+                    File.WriteAllLines(prezzi, prezziArray);
+                }
+                else if(scelta == 2)
+                {
+                    File.AppendAllLines(prezzi, prezziArray);
+                }
+            }
+            else
+            {
+                File.WriteAllLines(prezzi, prezziArray);
+            }
+                  
             
-            File.WriteAllLines(prezzi, prezziArray);    
 
 
 
