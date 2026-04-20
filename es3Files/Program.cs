b57b07fd8993557f983d5883fbcacb59b6e4691f
belladonna.giacomo <belladonna.giacomo@HP-003>
2026-04-20 09:47:21 +0200

    mod

---
+++ es3Files/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace es3Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeri = "numeri.txt";
+            string[] n = { "1", "2", "3", "4", "5" };
+            List<string> lista = new List<string>();
+            File.WriteAllLines(numeri, n);
+            string[] numeriArray = File.ReadAllLines(numeri);
+            for (int i = 0; i < numeriArray.Length; i++)
+            {
+                
+                lista.Add(numeriArray[i]);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
