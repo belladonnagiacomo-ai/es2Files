39b135554f03f6afbc265a11eb9e0a49fab89d47
belladonna.giacomo <belladonna.giacomo@HP-003>
2026-04-20 08:56:59 +0200

    mod

--- es2Files/Program.cs
+++ es2Files/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace es2Files
+﻿using System.ComponentModel.Design;
+
+namespace es2Files
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] prezziArray = { "19.99", "23.95", "5.50", "15.99", "10.50" };
+            string prezzi = "prezzi.dat";
+
+            
+            File.WriteAllLines(prezzi, prezziArray);    
+
+
+
+
+
+
         }
     }
 }
